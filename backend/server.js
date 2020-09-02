a8b813c912556eb69fe630a377193da122b1e68b
richell <richell.bonfield12@gmail.com>
2020-09-01 20:02:19 -0500

    modified server file

--- backend/server.js
+++ backend/server.js
@@ -39,12 +39,14 @@ app.get("/api/products", (req, res) => {
     res.send(data.products);
 }); 
 
+
+app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 app.use(express.static(path.join(__dirname, '/../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
-});
+
+let port = process.env.PORT || 5000
+app.listen(port, () => {console.log("Server started at port") })
 
