3dfe7acc38f94ea05dee48e16206d283d60c32c1
richell <richell.bonfield12@gmail.com>
2020-10-31 23:48:43 -0500

    reduced slug size

--- frontend/src/contexts/songs.js
+++ frontend/src/contexts/songs.js
@@ -1,14 +1,14 @@
 import beat1 from '../beatclips/davu_untagged_mp3.mp3';
-import beat2 from '../beatclips/jewry_tagged_mp3.mp3';
-import beat3 from '../beatclips/made_it_tagged_mp3.mp3';
-import beat4 from '../beatclips/nuisance_tagged_mp3.mp3';
-import beat5 from '../beatclips/pages_tagged_mp3.mp3';
+//import beat2 from '../beatclips/jewry_tagged_mp3.mp3';
+//import beat3 from '../beatclips/made_it_tagged_mp3.mp3';
+//import beat4 from '../beatclips/nuisance_tagged_mp3.mp3';
+//import beat5 from '../beatclips/pages_tagged_mp3.mp3';
 
 export const songsArr = [
     [
       "Davu Instrumental",
       beat1
-    ],
+    ], /*
     [
       "Jewry",
       beat2
@@ -24,5 +24,5 @@ export const songsArr = [
     [
       "Pages",
       beat5
-    ],
-  ];
+    ], */
+  ]; 
